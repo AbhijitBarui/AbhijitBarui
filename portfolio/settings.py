b8b21fe0df4bf63c29663bc3219fbe033805ff09
AbhijitBarui <abhijitongit@gmail.com>
2022-05-22 19:08:10 +0530

    allowhip

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jo-m3%%j=pcliu($!5-4f5z-5)t$dwg&yct^63rxcmlw2bdvn4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['54.202.139.54',]
 
 
 # Application definition
