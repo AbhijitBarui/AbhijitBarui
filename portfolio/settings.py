d5361baf86b6ffce00d8748ffce925ab51a55adf
AbhijitBarui <abhijitongit@gmail.com>
2022-05-23 09:16:03 +0530

    static inclusion careful

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jo-m3%%j=pcliu($!5-4f5z-5)t$dwg&yct^63rxcmlw2bdvn4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.202.139.54',]
+ALLOWED_HOSTS = ['54.202.139.54','127.0.0.1']
 
 
 # Application definition
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
