cf5e64cd49b54249873f9efa0560a4391ab80429
AbhijitBarui <abhijitongit@gmail.com>
2022-05-28 16:56:15 +0530

    urlshort adjusted

--- urlshortener/urls.py
+++ urlshortener/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
+    path('',views.index,name="urlshort"),
     path('create', views.create, name='create'),
     path('<str:slug>',views.real, name='real'),
 ]
