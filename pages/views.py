c42a447fb636cc80d1b7d744736ea458d87b7032
Abhijit-py <barui240@gmail.com>
2022-06-02 22:23:23 +0530

    index blog render

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from projects.models import Project
 from testimonials.models import Testimonial
+from blogs.models import Blog
 
 colors = ['blue','green','red','orange','purple','pink']
 
 def index(request):
     projects = Project.objects.all().order_by('-created')[:3]
     testimonials = Testimonial.objects.all().order_by('-created')
+    blogs = Blog.objects.all().order_by('-created').filter(is_published = True)[:3]
     context = {
         'projects': projects,
         'testimonials': testimonials,
+        'blogs': blogs,
         'color': colors,
     }
     return render(request, 'pages/index.html', context)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,27 +80,27 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'portfoliodb',
-#         'USER': 'abhijit',
-#         'PASSWORD': '76183492',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tutorialdb',
-        'USER': 'tutorialuser',
+        'NAME': 'portfoliodb',
+        'USER': 'abhijit',
         'PASSWORD': '76183492',
-        'HOST': 'database-1.chfvu3zacbxp.us-west-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tutorialdb',
+#         'USER': 'tutorialuser',
+#         'PASSWORD': '76183492',
+#         'HOST': 'database-1.chfvu3zacbxp.us-west-2.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 
